6968d7f9f1b792a5fc6fe7248c3aa9a58709602e
CoDIaCx2356 <soderstjernagustav@gmail.com>
2025-03-05 10:10:59 +0100

    Added changes to main bla bla

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("Hello, Git!");
         System.out.println("Version 2");
         System.out.println("Third change to code");
-        System.out.println("hej ho");
+        System.out.println("hej ho 13456");
         System.out.println("Bing Bang Boom");
     }
 }
