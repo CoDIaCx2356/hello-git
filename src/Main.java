2aa003f5270fed7ba638ab95fb5acec26db35ce6
CoDIaCx2356 <soderstjernagustav@gmail.com>
2025-03-05 10:03:39 +0100

    Added changes to main method

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Version 2");
         System.out.println("Third change to code");
         System.out.println("hej ho");
+        System.out.println("Bing Bang Boom");
     }
 }
