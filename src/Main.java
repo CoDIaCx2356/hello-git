6ffb0420d21ea056268f6ec4b0895278b790194b
Manden9753 <Andr9753@gmail.com>
2025-03-05 10:01:39 +0100

    Added change

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello, Git!");
         System.out.println("Version 2");
         System.out.println("Third change to code");
+        System.out.println("hej ho");
     }
 }
