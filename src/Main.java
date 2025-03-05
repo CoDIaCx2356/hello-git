d88eec345c7cac93e6719b342b8fe76620b50dac
CoDIaCx2356 <soderstjernagustav@gmail.com>
2025-03-05 09:12:52 +0100

    Updated Main.java with a new change

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
         System.out.println("Version 2");
+        System.out.println("Third change to code");
     }
 }
